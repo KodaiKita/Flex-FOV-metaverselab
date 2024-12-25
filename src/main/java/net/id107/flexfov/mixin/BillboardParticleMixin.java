8bccd27bf70d22cdeae7f67cbba49ab972c2e70c
Koddedai <87924564+Koddedai@users.noreply.github.com>
2024-12-26 02:50:30 +0900

    Vector3f to Vec3f

--- src/main/java/net/id107/flexfov/mixin/BillboardParticleMixin.java
+++ src/main/java/net/id107/flexfov/mixin/BillboardParticleMixin.java
@@ -1,5 +1,6 @@
 package net.id107.flexfov.mixin;
 
+import net.minecraft.util.math.Vec3f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -8,7 +9,6 @@ import net.id107.flexfov.projection.Projection;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.particle.BillboardParticle;
 import net.minecraft.client.particle.Particle;
-import net.minecraft.client.util.math.Vector3f;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.Quaternion;
@@ -24,7 +24,7 @@ public abstract class BillboardParticleMixin extends Particle {
 	@ModifyVariable(method = "buildGeometry(Lnet/minecraft/client/render/VertexConsumer;Lnet/minecraft/client/render/Camera;F)V",
 			ordinal = 0,
 			at = @At(value = "INVOKE_ASSIGN", ordinal = 0,
-			target = "Lnet/minecraft/client/render/Camera;getRotation()Lnet/minecraft/util/math/Quaternion;"))
+					target = "Lnet/minecraft/client/render/Camera;getRotation()Lnet/minecraft/util/math/Quaternion;"))
 	private Quaternion rotateParticle(Quaternion quaternion) {
 		if (Projection.getProjection().shouldRotateParticles()) {
 			MinecraftClient mc = MinecraftClient.getInstance();
@@ -33,8 +33,8 @@ public abstract class BillboardParticleMixin extends Particle {
 			Vec3d particlePos = new Vec3d(x, y, z).subtract(new Vec3d(prevPosX, prevPosY, prevPosZ)).multiply(Projection.getTickDelta()).add(new Vec3d(prevPosX, prevPosY, prevPosZ));
 			Vec3d dir = cameraPos.subtract(particlePos).normalize();
 			quaternion.set(0, 0, 0, 1);
-			quaternion.hamiltonProduct(Vector3f.POSITIVE_Y.getRadialQuaternion((float)Math.atan2(-dir.x, -dir.z)));
-			quaternion.hamiltonProduct(Vector3f.POSITIVE_X.getRadialQuaternion((float)Math.asin(dir.y)));
+			quaternion.hamiltonProduct(Vec3f.POSITIVE_Y.getRadialQuaternion((float)Math.atan2(-dir.x, -dir.z)));
+			quaternion.hamiltonProduct(Vec3f.POSITIVE_X.getRadialQuaternion((float)Math.asin(dir.y)));
 		}
 		return quaternion;
 	}
