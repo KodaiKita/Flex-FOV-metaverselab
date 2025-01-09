ac5005e86d02adbc0b6798bfb9bea0606de3bc2b
Koddedai <87924564+Koddedai@users.noreply.github.com>
2025-01-10 05:59:00 +0900

    loadUniform override and removed some waste code

--- src/main/java/net/id107/flexfov/projection/Cubic.java
+++ src/main/java/net/id107/flexfov/projection/Cubic.java
@@ -3,10 +3,12 @@ package net.id107.flexfov.projection;
 import net.id107.flexfov.BufferManager;
 import net.id107.flexfov.Reader;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.util.Window;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.Matrix4f;
 import net.minecraft.util.math.Quaternion;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL20;
 
 public class Cubic extends Projection {
 
@@ -96,5 +98,67 @@ public class Cubic extends Projection {
 		mc.worldRenderer.scheduleTerrainUpdate();
 
 		mc.options.hudHidden = hudHidden;
+	}
+
+	public void loadUniforms(float tickDelta) {
+		MinecraftClient mc = MinecraftClient.getInstance();
+		int shaderProgram = shader.getShaderProgram();
+		int displayWidth = MinecraftClient.getInstance().getWindow().getWidth();
+		int displayHeight = MinecraftClient.getInstance().getWindow().getHeight();
+		GL20.glUseProgram(shaderProgram);
+
+		int aaUniform = GL20.glGetUniformLocation(shaderProgram, "antialiasing");
+		GL20.glUniform1i(aaUniform, getAntialiasing());
+		int pixelOffestUniform;
+		System.out.println("[ANTI-ALIASING] " + getAntialiasing());
+
+		float left = (-1f+0.25f)/displayWidth;
+		float top = (-1f+0.25f)/displayHeight;
+		float right = 0.5f/displayWidth;
+		float bottom = 0.5f/displayHeight;
+		for (int y = 0; y < 4; y++) {
+			for (int x = 0; x < 4; x++) {
+				pixelOffestUniform = GL20.glGetUniformLocation(shaderProgram, "pixelOffset[" + (y*4+x) + "]");
+				GL20.glUniform2f(pixelOffestUniform, left + right*x, top + bottom*y);
+			}
+		}
+
+		int texUniform;
+
+		texUniform = GL20.glGetUniformLocation(shaderProgram, "texFront");
+		GL20.glUniform1i(texUniform, 0);
+
+		texUniform = GL20.glGetUniformLocation(shaderProgram, "texLeft");
+		GL20.glUniform1i(texUniform, 2);
+
+		texUniform = GL20.glGetUniformLocation(shaderProgram, "texRight");
+		GL20.glUniform1i(texUniform, 1);
+
+		texUniform = GL20.glGetUniformLocation(shaderProgram, "texBottom");
+		GL20.glUniform1i(texUniform, 3);
+
+		int fovxUniform = GL20.glGetUniformLocation(shaderProgram, "fovx");
+		GL20.glUniform1f(fovxUniform, (float) getFovX());
+
+		int fovyUniform = GL20.glGetUniformLocation(shaderProgram, "fovy");
+		GL20.glUniform1f(fovyUniform, (float) getFovY());
+
+		int backgroundUniform = GL20.glGetUniformLocation(shaderProgram, "backgroundColor");
+
+		// Black background
+		GL20.glUniform4f(backgroundUniform, 0, 0, 0, 1);
+
+		int zoomUniform = GL20.glGetUniformLocation(shaderProgram, "zoom");
+		GL20.glUniform1f(zoomUniform, (float)Math.pow(2, -zoom));
+
+		int drawCursorUniform = GL20.glGetUniformLocation(shaderProgram, "drawCursor");
+		GL20.glUniform1i(drawCursorUniform, (getResizeGui() && mc.currentScreen != null) ? 1 : 0);
+		int cursorPosUniform = GL20.glGetUniformLocation(shaderProgram, "cursorPos");
+		Window window = mc.getWindow();
+		float mouseX = (float)mc.mouse.getX() / (float)window.getWidth();
+		float mouseY = (float)mc.mouse.getY() / (float)window.getHeight();
+		mouseX = (mouseX - 0.5f) * window.getWidth() / (float)window.getHeight() + 0.5f;
+		mouseX = Math.max(0, Math.min(1, mouseX));
+		GL20.glUniform2f(cursorPosUniform, mouseX, 1-mouseY);
 	}
 }
